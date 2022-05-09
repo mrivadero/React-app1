b0b7e7b7b63217eacbb981151df92b6c813e4e1e
mrivadero <matias_sf17@hotmail.com>
2022-05-09 11:34:57 -0300

    React calc

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+
+//importar el componente
+
+//Utilizar el componente 
+
+//ReactDOM.render(<h1>Hola Mundo (App)</h1>,document.getElementById(`root`))
+
+ReactDOM.render(<App/>,document.getElementById(`root`))
